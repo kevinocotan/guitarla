a5ee289a0b0334de5951b600c21ba22f64f565c4
kevinocotan <kaocotan@gmail.com>
2024-12-10 16:39:49 -0600

    evento en jsx

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -2,6 +2,11 @@ export default function Guitar({guitar}) {
 
   const { id, name, image, description, price } = guitar;
 
+  const handleClick = (id) => {
+    console.log('Diste click', (id));
+    
+  }
+
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
       <div className="col-4">
@@ -17,8 +22,10 @@ export default function Guitar({guitar}) {
           {description}
         </p>
         <p className="fw-black text-primary fs-3">$ {price}</p>
-        <button type="button" className="btn btn-dark w-100">
-          Agregar al Carrito
+        <button type="button" 
+            className="btn btn-dark w-100"
+            onClick={() => handleClick(guitar)}
+        >Agregar al Carrito
         </button>
       </div>
     </div>
