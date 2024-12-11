dbff3c477de9861feff0c1205c3b6b5be19e2f33
kevinocotan <kaocotan@gmail.com>
2024-12-11 13:55:14 -0600

    creando el hook de useCart

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import Guitar from "./components/Guitar";
 import Header from "./components/Header";
-import { db } from "./data/db";
+import { db } from "./data/db"
+import { useCart } from "./hooks/useCart";
 
 function App() {
+
+  useCart()
 
   const initialCart = () => {
     const localStorageCart = localStorage.getItem('cart')
